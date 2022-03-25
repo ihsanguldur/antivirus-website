a8499767537f88256540c48b9785deec91babd29
ihsanguldur <ihsanguldur@outlook.com>
2022-03-25 20:01:33 +0300

    header and footer img is change screen gte 640 now.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
-import footerimg from "../resource/images/footer.jpg";
+import footer from "../resource/images/footer.jpg";
 import {BsFacebook, BsInstagram, BsTwitter} from "react-icons/bs";
+import min640Footer from "../resource/images/min640Footer.jpg";
 
 export default class Footer extends Component{
 
@@ -8,25 +9,27 @@ export default class Footer extends Component{
         return(
             <div>
                 <div className={"relative"}>
-                    <img
-                        src={footerimg}
-                        alt={"footerImg"}/>
+                    <picture>
+                        <source media={"(min-width : 640px)"} srcSet={footer}/>
+                        <img src={min640Footer} alt={"min640Footer"}/>
+                    </picture>
+
                     <div
                         className={"absolute top-1/4"}
                          style={{left:"15%"}}>
-                        <p className={"text-white font-bold lg:text-4xl md:text-3xl sm:text-2xl text-lg lg:mb-5 mb-2"}>
+                        <p className={"text-white font-bold lg:text-4xl md:text-3xl text-2xl lg:mb-5 mb-2"}>
                             Serphenix
                         </p>
-                        <div className={"text-white lg:text-xl md:text-lg sm:text-sm text-xs"}>
+                        <div className={"text-white lg:text-xl md:text-lg text-sm"}>
                             Contact Us
                         </div>
-                        <div className={"text-white lg:text-xl md:text-lg sm:text-sm text-xs flex mt-3"}>
+                        <div className={"text-white lg:text-xl md:text-lg text-sm flex mt-3"}>
                             <BsFacebook className={"mx-2 cursor-pointer"}/>
                             <BsInstagram className={"mx-2 cursor-pointer"}/>
                             <BsTwitter className={"mx-2 cursor-pointer"}/>
                         </div>
                     </div>
-                    <div className={"absolute w-full text-white lg:text-xl md:text-lg sm:text-sm text-xs bottom-0 mb-2 text-center"}>
+                    <div className={"absolute w-full text-white lg:text-xl md:text-lg text-sm bottom-0 mb-2 text-center"}>
                         copyright © 2002-2022 serphenix
                     </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import Nav from "./Nav";
 import background from "../resource/images/header.jpg";
-import NavHeader from "./Nav";
+import min640Header from "../resource/images/min640Header.jpg";
 
 export default class Header extends Component{
 
@@ -10,12 +10,15 @@ export default class Header extends Component{
             <div>
                 <Nav />
                 <div className={"relative"}>
-                    <img src={background} alt={"headerBackground"}/>
-                    <div className={"absolute top-1/3 left-2/3"}>
-                        <p className={"text-white font-bold lg:text-4xl md:text-3xl sm:text-2xl text-lg mb-5"}>
+                    <picture>
+                        <source media={"(min-width : 640px)"} srcSet={background}/>
+                        <img src={min640Header} alt={"min640Header"}/>
+                    </picture>
+                    <div className={"absolute sm:top-1/3 sm:left-2/3 sm:translate-x-0 sm:translate-y-0 text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"}>
+                        <p className={"text-white font-bold lg:text-4xl md:text-3xl text-2xl mb-5"}>
                             World Leader in Cyber Security
                         </p>
-                        <div className={"text-white lg:text-xl md:text-lg sm:text-sm text-xs"}>
+                        <div className={"text-white lg:text-xl md:text-lg text-sm"}>
                             We've been protecting you and your family for 20 years...
                         </div>
                     </div>
