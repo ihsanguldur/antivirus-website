35bccfc8ba104e388119ae4207587931502d2964
ihsanguldur <ihsanguldur@outlook.com>
2022-05-20 00:02:37 +0300

    user profile fixed.

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -4,6 +4,8 @@ import {bindActionCreators} from "redux";
 import {getUser, updateUser} from "../services/redux/actions/userActions";
 import Nav from "../components/Nav";
 import WarningAlert from "../components/helpers/WarningAlert";
+import ErrorAlert from "../components/helpers/ErrorAlert";
+import {isEmailValid} from "../utils/validation";
 
 class UserProfile extends Component{
 
@@ -13,8 +15,16 @@ class UserProfile extends Component{
         }
     }
 
-    state={onEdit : false,isHidden: true}
 
+    setUpStates(...states) {
+        states.forEach((s)=>{
+            this.setState(s);
+        });
+    }
+
+
+
+    state={onEdit : false,isHidden: true, name : undefined,surname : undefined, email : undefined, errMsg : "", err : true}
 
     renderBeforeSave() {
         return (
@@ -40,23 +50,11 @@ class UserProfile extends Component{
                     <button
                         className={"animate lg:text-xl md:text-sm text-xs bg-orange-500 hover:bg-orange-600 text-white text-center rounded-lg lg:py-3 lg:px-5 py-2 px-3 absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2"}
                         onClick={() => {
-                            let name = document.getElementById("name").value;
-                            let email = document.getElementById("email").value;
-                            if(!name.includes(" ")){
-                                alert("aga kontrol et.");
-                            }else{
-                                let nameArr = name.split(" ");
-                                if(nameArr.length === 1){
-                                    nameArr[0] = this.props.user.data.name;
-                                    nameArr[1] = this.props.user.data.surname;
-                                }
-                                if(email === ""){
-                                    email = this.props.user.data.email;
-                                }
-                                this.props.actions.updateUser(this.props.user.data._id, {name : nameArr[0], surname : nameArr[1], email : email});
-                            }
-                            //name.split(" ");
-                            console.log(name.split(" "), email);
+                            let name = document.getElementById("name").value.trim();
+                            let surname = document.getElementById("surname").value.trim();
+                            let email = document.getElementById("email").value.trim();
+                            this.props.actions.updateUser(this.props.user.data._id, {name : name, surname : surname, email : email});
+                            this.setState({err : true});
                             this.setState({onEdit : false});
                             this.setState({isHidden: true});
                         }}>
@@ -78,11 +76,84 @@ class UserProfile extends Component{
         );
     }
 
+    renderNameInput(){
+        return (
+            <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
+                <span className={"font-bold text-orange-400"}>Name : </span>
+                <input
+                    className={"placeholder-black " + (this.state.onEdit?" focus:outline-orange-400":"focus:outline-none")}
+                    id={"name"}
+                    readOnly={!this.state.onEdit}
+                    onChange={(event)=>{
+                        this.setState({name : event.target.value});
+                    }}
+                    type={"text"}
+                    //placeholder={this.props.user.data.name +" "+ this.props.user.data.surname}
+                    value={this.state.name === undefined?this.props.user.data.name:this.state.name}/>
+                {/*this.props.user.data.name +" "+ this.props.user.data.surname*/}
+            </p>
+        )
+    }
+
+    renderSurnameInput(){
+        return(
+            <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
+                <span className={"font-bold text-orange-400"}>Surname : </span>
+                <input
+                    className={"placeholder-black " + (this.state.onEdit?" focus:outline-orange-400":"focus:outline-none")}
+                    id={"surname"}
+                    readOnly={!this.state.onEdit}
+                    onChange={(event)=>{
+                        this.setState({surname : event.target.value});
+                    }}
+                    type={"text"}
+                    //placeholder={this.props.user.data.name +" "+ this.props.user.data.surname}
+                    value={this.state.surname === undefined?this.props.user.data.surname:this.state.surname}/>
+                {/*this.props.user.data.name +" "+ this.props.user.data.surname*/}
+            </p>
+        )
+    }
+
+    renderEmailInput(){
+        return(
+            <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
+                <span className={"font-bold text-orange-400"}>Email : </span>
+                <input
+                    className={"placeholder-black "+ (this.state.onEdit?" focus:outline-orange-400":"focus:outline-none")}
+                    id={"email"}
+                    readOnly={!this.state.onEdit}
+                    onChange={(event)=>{
+                        this.setState({email : event.target.value});
+                    }}
+                    type={"email"}
+                    value={this.state.email === undefined?this.props.user.data.email:this.state.email}/>
+                {/*this.props.user.data.email*/}
+            </p>
+        )
+    }
+
+    renderMembership(){
+        return (
+            <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
+                <span className={"font-bold text-orange-400"}>Membership : </span>
+                {this.props.user.data.membershipName}
+            </p>
+        )
+    }
+
+    renderCreatedAt(){
+        return(
+            <p className={"my-3 xl:text-xl lg:text-lg md:text-base"}>
+                <span className={"font-bold text-orange-400"}> Create Date : </span>
+                {this.props.user.data.createdAt?this.props.user.data.createdAt.substring(0,10):""}
+            </p>
+        )
+    }
+
     render(){
         return(
             <div className={"h-screen grid grid-cols-12 flex-1 border-x border-orange-400"}>
                 {this.renderOverlay()}
-
                 {this.props.isUser&&(<Nav isAnother={true}/>)}
                 <div className={"md:bg-orange-400 bg-transparent -z-50 absolute h-screen w-1/4 text-transparent"}/>
                 <img
@@ -90,53 +161,61 @@ class UserProfile extends Component{
                     src={"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
                     alt={"customer_img"}/>
                 <div className={"md:col-start-6 md:col-span-4 justify-self-center md:self-center col-span-12"}>
-                    <div className={"my-10 "}>
+                    <ErrorAlert
+                        isValid={this.state.err}
+                        message={this.state.errMsg}/>
+                    <div className={"mb-10 mt-5"}>
                         <WarningAlert isValid={this.state.onEdit} message={"You are on edit!"} />
                     </div>
-                    <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
-                        <span className={"font-bold text-orange-400"}>Name : </span>
-                        <input
-                            className={"placeholder-black " + (this.state.onEdit?" focus:outline-orange-400":"focus:outline-none")}
-                            id={"name"}
-                            readOnly={!this.state.onEdit}
-                            onChange={(e)=>{
-                                console.log(e.target.value)
-                            }}
-                            type={"text"}
-                            placeholder={this.props.user.data.name +" "+ this.props.user.data.surname}/>
-                        {/*this.props.user.data.name +" "+ this.props.user.data.surname*/}
-                    </p>
-                    <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
-                        <span className={"font-bold text-orange-400"}>Email : </span>
-                        <input
-                            className={"placeholder-black "+ (this.state.onEdit?" focus:outline-orange-400":"focus:outline-none")}
-                            id={"email"}
-                            readOnly={!this.state.onEdit}
-                            type={"email"}
-                            placeholder={this.props.user.data.email}/>
-                        {/*this.props.user.data.email*/}
-                    </p>
-                    {this.props.isUser&&
-                        <p className={"my-3 xl:text-xl lg:text-lg md:text-base text-sm"}>
-                            <span className={"font-bold text-orange-400"}>Membership : </span>
-                            {this.props.user.data.membershipName}
-                    </p>}
-                    <p className={"my-3 xl:text-xl lg:text-lg md:text-base"}>
-                        <span className={"font-bold text-orange-400"}> Create Date : </span>
-                        {this.props.user.data.createdAt?this.props.user.data.createdAt.substring(0,10):""}
-                    </p>
+                    {this.renderNameInput()}
+                    {this.renderSurnameInput()}
+                    {this.renderEmailInput()}
+                    {this.props.isUser && this.renderMembership()}
+                    {this.renderCreatedAt()}
                     <button
                         className={"animate bg-orange-500 hover:bg-orange-600 mt-5 text-white font-bold py-2 px-6 rounded-md focus:outline-none xl:text-xl lg:text-lg md:text-base text-sm h-fit"}
                         type={"button"}
                         onClick={()=>{
                             if(this.state.onEdit){
-                                this.setState({isHidden : false});
+                                let name = document.getElementById("name").value.trim();
+                                let surname = document.getElementById("surname").value.trim();
+                                let email = document.getElementById("email").value.trim();
+                                if(name === "" || surname === "" || email === "") {
+                                    this.setState({errMsg: "Please Check Your Inputs."})
+                                    this.setState({err: false});
+                                }else if(!isEmailValid(email)){
+                                    this.setState({errMsg: "Email is not valid."})
+                                    this.setState({err: false});
+                                }else{
+                                    this.setState({isHidden : false});
+                                }
                             }else{
-                                this.setState({onEdit : !this.state.onEdit})
+                                this.setState({onEdit : !this.state.onEdit});
+                                this.setState({err : true});
                             }
                         }}>
                         {this.state.onEdit?"Save":"Edit"}
                     </button>
+                    {this.state.onEdit && <button
+                        className={"ml-5 animate bg-orange-500 hover:bg-orange-600 mt-5 text-white font-bold py-2 px-6 rounded-md focus:outline-none xl:text-xl lg:text-lg md:text-base text-sm h-fit"}
+                        type={"button"}
+                        onClick={()=>{
+
+                            //deneme fatihe bir sor ona göre tamamına uygula.
+                            this.setUpStates(
+                                {onEdit : !this.state.onEdit},
+                                {err : true},
+                                {name : undefined},
+                                {email : undefined}
+                            )
+                            /*this.setState({onEdit : !this.state.onEdit});
+                            this.setState({err : true});
+                            this.setState({name : undefined});
+                            this.setState({surname : undefined});
+                            this.setState({email : undefined});*/
+                        }}>
+                        Cancel
+                    </button>}
                 </div>
 
             </div>
--- src/services/redux/reducers/userReducer.js
+++ src/services/redux/reducers/userReducer.js
@@ -7,7 +7,7 @@ export default function userReducer(state = initialStates.temp,action){
         case actionTypes.GET_USER :
             let newState;
             if(action.payload){
-                newState = {success : "", data : action.payload}
+                newState = {success : true, data : action.payload}
             }else {
                 newState = state;
             }
@@ -20,7 +20,7 @@ export default function userReducer(state = initialStates.temp,action){
                 newState1 = {success : action.payload.success, data : data};
                 localStorage.setItem("user", JSON.stringify(data));
             }else {
-                newState1 = {success:false, data : user};
+                newState1 = {success:false, data : user, message : action.payload.message};
             }
             console.log(newState1)
             return newState1;
