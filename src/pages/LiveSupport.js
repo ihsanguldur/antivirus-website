e16b88aa2ab457747e88411ee0a89c52306165ec
ihsanguldur <ihsanguldur@outlook.com>
2022-05-25 22:04:32 +0300

    loading screen added

--- src/pages/LiveSupport.js
+++ src/pages/LiveSupport.js
@@ -7,7 +7,7 @@ import {connect} from "react-redux";
 class LiveSupport extends Component {
 
     socket;
-
+    state = {deneme : false}
     componentDidMount() {
         //bir socket bağlantısı oluşturduk.
         this.socket = io.connect("http://localhost:5000");
@@ -21,9 +21,87 @@ class LiveSupport extends Component {
         });
     }
 
-    render() {
+    /*renderBefore() {
         return (
-            <div className={"container mx-auto"}>
+            <div>
+                <div
+                    className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg lg:py-24 lg:px-12 py-12 px-4"}>
+                    <svg
+                        xmlns={"http://www.w3.org/2000/svg"}
+                        onClick={() => {
+                            this.setState({isHidden: !this.state.isHidden});
+                        }}
+                        className={"lg:h-6 lg:w-6 md:h-5 md:w-5 h-4 w-4 absolute top-5 right-5 cursor-pointer"}
+                        fill={"none"}
+                        viewBox={"0 0 24 24"}
+                        stroke={"currentColor"}
+                        strokeWidth={2}>
+                        <path
+                            strokeLinecap={"round"}
+                            strokeLinejoin={"round"}
+                            d={"M6 18L18 6M6 6l12 12"}/>
+                    </svg>
+                    <p className={"lg:text-xl md:text-sm text-xs text-center my-3"}>Are you sure you want to save?</p>
+                    <button
+                        type="button"
+                        className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed "
+                        disabled>
+                        <svg
+                            className="animate-spin h-5 w-5 mr-3 "
+                            viewBox="0 0 24 24">
+                            <circle
+                                className={"opacity-25"}
+                                cx={12}
+                                cy={12}
+                                r={10}
+                                stroke={"currentColor"}
+                                strokeWidth={4}
+                            />
+                            <path
+                                className={"opacity-75"}
+                                fill={"currentColor"}
+                                d={"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"}
+                            />
+                        </svg>
+                        Processing...
+                    </button>
+                </div>
+            </div>
+
+        );
+    }*/
+
+    renderWaitingScreen() {
+        return (
+            <div
+                id={"overlay"}
+                className={"bg-gray-900 w-screen h-screen "+ (this.state.isHidden ? "hidden" : "")}>
+                <span className={"top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute text-orange-500 lg:text-base md:text-sm text-xs text-center"}>
+                    <svg
+                        className="animate-spin h-5 w-5 md:inline-block md:mr-3 mx-auto md:mb-0 mb-5"
+                        viewBox="0 0 24 24">
+                            <circle
+                                className={"opacity-25"}
+                                cx={12}
+                                cy={12}
+                                r={10}
+                                stroke={"currentColor"}
+                                strokeWidth={4}
+                            />
+                            <path
+                                className={"opacity-75"}
+                                fill={"currentColor"}
+                                d={"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"}
+                            />
+                        </svg>
+                            Please wait a bit. We will contact you immediately.
+                </span>
+            </div>
+        );
+    }
+
+    renderSupportChat(){
+        return (<div className={"container mx-auto"}>
                 <div className={"flex flex-col rounded-lg w-5/6 mx-auto absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-xl"}>
                     {/*live chat header*/}
                     <div className={"flex items-center bg-gradient-to-r from-orange-400 to-orange-600 rounded-t-2xl py-3"}>
@@ -62,6 +140,13 @@ class LiveSupport extends Component {
 
                 </div>
             </div>
+
+        )
+    }
+
+    render() {
+        return (
+            this.state.deneme?this.renderSupportChat():this.renderWaitingScreen()
         );
     }
 
